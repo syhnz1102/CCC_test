4f5bc4a103a0bba6c5f735e70ed2a399c59d3c8f
ivypark <ivypark@amoeba.co.kr>
2020-07-13 16:44:42 +0900

    [Released] v1.0.6 (0713)

--- src/config.js
+++ src/config.js
@@ -1,9 +1,8 @@
 export default {
-  version: 'v1.0.5 (0709)',
+  version: 'v1.0.6 (0713)',
   // socketIoUrl: 'https://ktgenie.com:7103/SignalServer', // TB
   // socketIoUrl: 'https://localhost:7103/SignalServer', // LOCAL
   socketIoUrl: 'https://cococall.net:7103/SignalServer', // RELEASED
-  // iceServer: { iceServers: [{ urls: 'turn:106.240.247.44:46000', username: 'kpoint', credential: 'kpoint01' }] }, // TB, LOCAL
   iceServer: { iceServers: [{ urls: 'turn:13.125.217.175:46000', username: 'kpoint', credential: 'kpoint01' }, { urls : 'stun:13.125.217.175:46000' }, { urls: 'turn:106.240.247.44:46000', username: 'kpoint', credential: 'kpoint01' }] }, // RELEASED
   constraints: {
     share: { width: 480, height: 270 }, // video constraint when screen sharing
