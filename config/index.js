0df52292e2207edc76e79f8e3959c6691a113025
ivypark <ivypark@amoeba.co.kr>
2020-08-28 16:40:45 +0900

    RELEASED to PROD v1.1.1-rc3

--- config/index.js
+++ config/index.js
@@ -25,7 +25,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 8001,
+    port: 443,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
