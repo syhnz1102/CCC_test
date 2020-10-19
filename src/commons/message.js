46d2ee848a3f5953d32d790b8370fa1b3d2d324d
mztothem <mjseo@knowledgepoint.co.kr>
2020-10-19 11:16:53 +0900

    화면 감지 message 'action' :' talking' 수정

--- src/commons/message.js
+++ src/commons/message.js
@@ -156,22 +156,20 @@ export async function onMessage(resp) {
       break;
 
     case 'Presence':
-      if (resp.hasOwnProperty('action')){
-        if (resp.action === 'exit') {
-          store.commit('setRoomInfo', { count: store.state.roomInfo.count - 1 });
-          if (store.state.roomInfo.count <= 1 && store.state.peerInfo.hasOwnProperty('screen')) {
-            eBus.$emit('share', {
-              type: 'remove',
-              isSharer: true
-            })
-            eBus.$emit('popup', {
-              on: true,
-              type: 'Alert',
-              title: '화면 공유',
-              contents: '다른 모든 사용자가 통화를 종료하여 화면공유가 종료됩니다.'
-            })
-          }
-
+      if (resp.action === 'exit') {
+        store.commit('setRoomInfo', { count: store.state.roomInfo.count - 1 });
+        if (store.state.roomInfo.count <= 1 && store.state.peerInfo.hasOwnProperty('screen')) {
+          eBus.$emit('share', {
+            type: 'remove',
+            isSharer: true
+          })
+          eBus.$emit('popup', {
+            on: true,
+            type: 'Alert',
+            title: '화면 공유',
+            contents: '다른 모든 사용자가 통화를 종료하여 화면공유가 종료됩니다.'
+          })
+        }
           eBus.$emit('video', {
             type: 'remove',
             count: store.state.roomInfo.count,
@@ -179,16 +177,16 @@ export async function onMessage(resp) {
           })
         } else if (resp.action === 'join') {
           if (resp.members) store.commit('setRoomInfo', { members: resp.members, count: Object.keys(resp.members).length });
+        } else if (resp.action === 'talking') {
+          //화자감지 이벤트 수신
+          //action 추가 : talking
+          if (store.state.showsSpeaker && store.state.roomInfo.count > 2){
+            eBus.$emit('setVideo',{
+              id : resp.who === store.state.userInfo.id ? 'local' : resp.who,
+              isTalking : resp.talking
+            });
+          }
         }
-    }else if (resp.hasOwnProperty('who') && resp.hasOwnProperty('talking')){
-      //화자감지 이벤트 수신
-      if (store.state.showsSpeaker && store.state.roomInfo.count > 2){
-        eBus.$emit('setVideo',{
-          id : resp.who === store.state.userInfo.id ? 'local' : resp.who,
-          isTalking : resp.talking
-        });
-      }
-    }
       break;
 
     case 'ChangeName':
